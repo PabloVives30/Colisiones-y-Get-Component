d3a2f683f7d78e1a482c59a35f08e915b2f50322
PabloVives30 <48116412@est.ort.edu.ar>
2024-06-06 09:30:19 -0300

    Initial commit

---
+++ Assets/planeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeScript : MonoBehaviour
+{
+
+    public int resistencia;
+    public sphereScript sphere;
+
+    //cuando un objeto colisiones ocn este debe escribirse en la consola la planbra "Contacto"
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        sphere = collision.gameObject.GetComponent<sphereScript>();
+        Debug.Log("Contacto con una esfera que pesa " + sphere.peso);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Colisiones-y-Get-Component
+ 
